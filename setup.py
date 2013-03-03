f6cbeca4049a08047a6b4e7b88e15d4c6655524e
Steven Myint <git@stevenmyint.com>
2013-03-03 14:24:59 -0800

    Add missing name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.rst') as readme:
                description='Removes commented-out code.',
                long_description=readme.read(),
                license='Expat License',
-               author='myint',
+               author='Steven Myint',
                url='https://github.com/myint/eradicate',
                classifiers=['Intended Audience :: Developers',
                             'Environment :: Console',
