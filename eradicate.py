2035249472cea2e7404427d4c69ab2215777309e
Steven Myint <git@stevenmyint.com>
2013-04-13 17:26:20 -0700

    Handle broken pipe more elegantly

--- eradicate.py
+++ eradicate.py
@@ -181,6 +181,4 @@ def main(argv, standard_out, standard_error):
             try:
                 fix_file(name, args=args, standard_out=standard_out)
             except IOError as exception:
-                if exception.errno == errno.EPIPE:
-                    return
                 print(exception, file=standard_error)
