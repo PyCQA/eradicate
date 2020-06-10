7ac22160d80f296c4c74b40b499c56f3549f208b
Cielquan <cielquan@protonmail.com>
2020-06-10 17:19:50 +0200

    refactored functions are glob vars into a class

--- eradicate.py
+++ eradicate.py
@@ -32,185 +32,184 @@ import tokenize
 
 __version__ = '1.0'
 
+class Eradicator:
+    """Eradicate comments."""
+    MULTILINE_ASSIGNMENT_REGEX = re.compile(r'^\s*\w+\s*=.*[(\[{]$')
+    PARTIAL_DICTIONARY_REGEX = re.compile(r'^\s*[\'"]\w+[\'"]\s*:.+[,{]\s*$')
 
-MULTILINE_ASSIGNMENT_REGEX = re.compile(r'^\s*\w+\s*=.*[(\[{]$')
-PARTIAL_DICTIONARY_REGEX = re.compile(r'^\s*[\'"]\w+[\'"]\s*:.+[,{]\s*$')
+    DEFAULT_WHITELIST = [
+        r'pylint',
+        r'pyright',
+        r'noqa',
+        r'type:\s*ignore',
+        r'fmt:\s*(on|off)',
+        r'TODO',
+        r'FIXME',
+        r'XXX'
+    ]
+    WHITELIST_REGEX = re.compile(r'|'.join(DEFAULT_WHITELIST), flags=re.IGNORECASE)
 
+    def comment_contains_code(self, line, aggressive=True):
+        """Return True comment contains code."""
+        line = line.lstrip()
+        if not line.startswith('#'):
+            return False
 
-DEFAULT_WHITELIST = [
-    r'pylint',
-    r'pyright',
-    r'noqa',
-    r'type:\s*ignore',
-    r'fmt:\s*(on|off)',
-    r'TODO',
-    r'FIXME',
-    r'XXX'
-]
-WHITELIST_REGEX = re.compile(r'|'.join(DEFAULT_WHITELIST), flags=re.IGNORECASE)
+        line = line.lstrip(' \t\v\n#').strip()
 
+        # Ignore non-comment related hashes. For example, "# Issue #999".
+        if re.search('#[0-9]', line):
+            return False
 
-def comment_contains_code(line, aggressive=True):
-    """Return True comment contains code."""
-    line = line.lstrip()
-    if not line.startswith('#'):
-        return False
+        # Ignore whitelisted comments
+        if self.WHITELIST_REGEX.search(line):
+            return False
 
-    line = line.lstrip(' \t\v\n#').strip()
+        if re.match(r'.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)', line):
+            return False
 
-    # Ignore non-comment related hashes. For example, "# Issue #999".
-    if re.search('#[0-9]', line):
-        return False
-
-    # Ignore whitelisted comments
-    if WHITELIST_REGEX.search(line):
-        return False
-
-    if re.match(r'.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)', line):
-        return False
-
-    # Check that this is possibly code.
-    for symbol in list('()[]{}:=%') + ['print', 'return', 'break', 'continue',
-                                       'import']:
-        if symbol in line:
-            break
-    else:
-        return False
-
-    if multiline_case(line, aggressive=aggressive):
-        return True
-
-    symbol_list = [r'elif\s+.*', 'else', 'try',
-                   'finally', r'except\s+.*']
-    if aggressive:
-        symbol_list.append(r'if\s+.*')
-
-    for symbol in symbol_list:
-        if re.match(r'^\s*' + symbol + r'\s*:\s*$', line):
-            return True
-
-    line = re.sub(r'^(print|return)\b\s*', '', line)
-
-    if re.match(PARTIAL_DICTIONARY_REGEX, line):
-        return True
-
-    try:
-        compile(line, '<string>', 'exec')
-        return True
-    except (SyntaxError, TypeError, UnicodeDecodeError):
-        return False
-
-
-def multiline_case(line, aggressive=True):
-    """Return True if line is probably part of some multiline code."""
-    if aggressive:
-        for ending in ')]}':
-            if line.endswith(ending + ':'):
-                return True
-
-            if line.strip() == ending + ',':
-                return True
-
-        # Check whether a function/method definition with return value
-        # annotation
-        if re.search(r"def .+\)[\s]+->[\s]+[a-zA-Z_][a-zA-Z0-9_]*:$", line):
-            return True
-
-        # Check weather a with statement
-        if re.search(r"with .+ as [a-zA-Z_][a-zA-Z0-9_]*:$", line):
-            return True
-
-        # Check weather a for statement
-        if re.search(r"for [a-zA-Z_][a-zA-Z0-9_]* in .+:$", line):
-            return True
-
-    if line.endswith('\\'):
-        return True
-
-    if re.match(MULTILINE_ASSIGNMENT_REGEX, line):
-        return True
-
-    if re.match(r'^[()\[\]{}\s]+$', line):
-        return True
-
-    return False
-
-
-def commented_out_code_line_numbers(source, aggressive=True):
-    """Yield line numbers of commented-out code."""
-    sio = io.StringIO(source)
-    try:
-        for token in tokenize.generate_tokens(sio.readline):
-            token_type = token[0]
-            start_row = token[2][0]
-            line = token[4]
-
-            if (token_type == tokenize.COMMENT and
-                    line.lstrip().startswith('#') and
-                    comment_contains_code(line, aggressive)):
-                yield start_row
-    except (tokenize.TokenError, IndentationError):
-        pass
-
-
-def filter_commented_out_code(source, aggressive=True):
-    """Yield code with commented out code removed."""
-    marked_lines = list(commented_out_code_line_numbers(source,
-                                                        aggressive))
-    sio = io.StringIO(source)
-    previous_line = ''
-    for line_number, line in enumerate(sio.readlines(), start=1):
-        if (line_number not in marked_lines or
-                previous_line.rstrip().endswith('\\')):
-            yield line
-        previous_line = line
-
-
-def fix_file(filename, args, standard_out):
-    """Run filter_commented_out_code() on file."""
-    encoding = detect_encoding(filename)
-    with open_with_encoding(filename, encoding=encoding) as input_file:
-        source = input_file.read()
-
-    filtered_source = ''.join(filter_commented_out_code(source,
-                                                        args.aggressive))
-
-    if source != filtered_source:
-        if args.in_place:
-            with open_with_encoding(filename, mode='w',
-                                    encoding=encoding) as output_file:
-                output_file.write(filtered_source)
+        # Check that this is possibly code.
+        for symbol in list('()[]{}:=%') + ['print', 'return', 'break', 'continue',
+                                           'import']:
+            if symbol in line:
+                break
         else:
-            diff = difflib.unified_diff(
-                source.splitlines(),
-                filtered_source.splitlines(),
-                'before/' + filename,
-                'after/' + filename,
-                lineterm='')
-            standard_out.write('\n'.join(list(diff) + ['']))
-        return True
+            return False
+
+        if self.multiline_case(line, aggressive=aggressive):
+            return True
+
+        symbol_list = [r'elif\s+.*', 'else', 'try',
+                       'finally', r'except\s+.*']
+        if aggressive:
+            symbol_list.append(r'if\s+.*')
+
+        for symbol in symbol_list:
+            if re.match(r'^\s*' + symbol + r'\s*:\s*$', line):
+                return True
+
+        line = re.sub(r'^(print|return)\b\s*', '', line)
+
+        if re.match(self.PARTIAL_DICTIONARY_REGEX, line):
+            return True
+
+        try:
+            compile(line, '<string>', 'exec')
+            return True
+        except (SyntaxError, TypeError, UnicodeDecodeError):
+            return False
 
 
-def open_with_encoding(filename, encoding, mode='r'):
-    """Return opened file with a specific encoding."""
-    return io.open(filename, mode=mode, encoding=encoding,
-                   newline='')  # Preserve line endings
+    def multiline_case(self, line, aggressive=True):
+        """Return True if line is probably part of some multiline code."""
+        if aggressive:
+            for ending in ')]}':
+                if line.endswith(ending + ':'):
+                    return True
+
+                if line.strip() == ending + ',':
+                    return True
+
+            # Check whether a function/method definition with return value
+            # annotation
+            if re.search(r"def .+\)[\s]+->[\s]+[a-zA-Z_][a-zA-Z0-9_]*:$", line):
+                return True
+
+            # Check weather a with statement
+            if re.search(r"with .+ as [a-zA-Z_][a-zA-Z0-9_]*:$", line):
+                return True
+
+            # Check weather a for statement
+            if re.search(r"for [a-zA-Z_][a-zA-Z0-9_]* in .+:$", line):
+                return True
+
+        if line.endswith('\\'):
+            return True
+
+        if re.match(self.MULTILINE_ASSIGNMENT_REGEX, line):
+            return True
+
+        if re.match(r'^[()\[\]{}\s]+$', line):
+            return True
+
+        return False
 
 
-def detect_encoding(filename):
-    """Return file encoding."""
-    try:
-        with open(filename, 'rb') as input_file:
-            from lib2to3.pgen2 import tokenize as lib2to3_tokenize
-            encoding = lib2to3_tokenize.detect_encoding(input_file.readline)[0]
+    def commented_out_code_line_numbers(self, source, aggressive=True):
+        """Yield line numbers of commented-out code."""
+        sio = io.StringIO(source)
+        try:
+            for token in tokenize.generate_tokens(sio.readline):
+                token_type = token[0]
+                start_row = token[2][0]
+                line = token[4]
 
-            # Check for correctness of encoding.
-            with open_with_encoding(filename, encoding) as input_file:
-                input_file.read()
+                if (token_type == tokenize.COMMENT and
+                        line.lstrip().startswith('#') and
+                        self.comment_contains_code(line, aggressive)):
+                    yield start_row
+        except (tokenize.TokenError, IndentationError):
+            pass
 
-        return encoding
-    except (SyntaxError, LookupError, UnicodeDecodeError):
-        return 'latin-1'
+
+    def filter_commented_out_code(self, source, aggressive=True):
+        """Yield code with commented out code removed."""
+        marked_lines = list(self.commented_out_code_line_numbers(source,
+                                                            aggressive))
+        sio = io.StringIO(source)
+        previous_line = ''
+        for line_number, line in enumerate(sio.readlines(), start=1):
+            if (line_number not in marked_lines or
+                    previous_line.rstrip().endswith('\\')):
+                yield line
+            previous_line = line
+
+
+    def fix_file(self, filename, args, standard_out):
+        """Run filter_commented_out_code() on file."""
+        encoding = self.detect_encoding(filename)
+        with self.open_with_encoding(filename, encoding=encoding) as input_file:
+            source = input_file.read()
+
+        filtered_source = ''.join(self.filter_commented_out_code(source,
+                                                            args.aggressive))
+
+        if source != filtered_source:
+            if args.in_place:
+                with self.open_with_encoding(filename, mode='w',
+                                        encoding=encoding) as output_file:
+                    output_file.write(filtered_source)
+            else:
+                diff = difflib.unified_diff(
+                    source.splitlines(),
+                    filtered_source.splitlines(),
+                    'before/' + filename,
+                    'after/' + filename,
+                    lineterm='')
+                standard_out.write('\n'.join(list(diff) + ['']))
+            return True
+
+
+    def open_with_encoding(self, filename, encoding, mode='r'):
+        """Return opened file with a specific encoding."""
+        return io.open(filename, mode=mode, encoding=encoding,
+                       newline='')  # Preserve line endings
+
+
+    def detect_encoding(self, filename):
+        """Return file encoding."""
+        try:
+            with open(filename, 'rb') as input_file:
+                from lib2to3.pgen2 import tokenize as lib2to3_tokenize
+                encoding = lib2to3_tokenize.detect_encoding(input_file.readline)[0]
+
+                # Check for correctness of encoding.
+                with self.open_with_encoding(filename, encoding) as input_file:
+                    input_file.read()
+
+            return encoding
+        except (SyntaxError, LookupError, UnicodeDecodeError):
+            return 'latin-1'
 
 
 def main(argv, standard_out, standard_error):
@@ -233,25 +232,26 @@ def main(argv, standard_out, standard_error):
                             'String of "#" separated comment beginnings to whitelist. '
                             'Single parts are interpreted as regex. '
                             'OVERWRITING the default whitelist: {}'
-                        ).format(DEFAULT_WHITELIST))
+                        ).format(Eradicator.DEFAULT_WHITELIST))
     parser.add_argument('--whitelist-extend', action="store",
                         help=(
                             'String of "#" separated comment beginnings to whitelist '
                             'Single parts are interpreted as regex. '
                             'Overwrites --whitelist. '
                             'EXTENDING the default whitelist: {} '
-                        ).format(DEFAULT_WHITELIST))
+                        ).format(Eradicator.DEFAULT_WHITELIST))
     parser.add_argument('files', nargs='+', help='files to format')
 
     args = parser.parse_args(argv[1:])
 
-    global WHITELIST_REGEX
+    eradicator = Eradicator()
+
     if args.whitelist_extend:
-        WHITELIST_REGEX = re.compile(
-            r'|'.join(DEFAULT_WHITELIST + args.whitelist_extend.split('#')),
+        eradicator.WHITELIST_REGEX = re.compile(
+            r'|'.join(eradicator.DEFAULT_WHITELIST + args.whitelist_extend.split('#')),
             flags=re.IGNORECASE)
     elif args.whitelist:
-        WHITELIST_REGEX = re.compile(
+        eradicator.WHITELIST_REGEX = re.compile(
             r'|'.join(args.whitelist.split('#')),
             flags=re.IGNORECASE)
 
@@ -268,7 +268,7 @@ def main(argv, standard_out, standard_error):
                                   if not d.startswith('.')]
         else:
             try:
-                change_or_error = fix_file(name, args=args, standard_out=standard_out) or change_or_error
+                change_or_error = eradicator.fix_file(name, args=args, standard_out=standard_out) or change_or_error
             except IOError as exception:
                 print('{}'.format(exception), file=standard_error)
                 change_or_error = True
